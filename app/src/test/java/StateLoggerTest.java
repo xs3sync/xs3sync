9e3bb1230aa79bdef45943ae71c025bc87ae89cb
Paweł Bobryk <bobryk.pawel@gmail.com>
2025-11-18 16:10:37 +0100

    sync

--- app/src/test/java/StateLoggerTest.java
+++ app/src/test/java/StateLoggerTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StateLoggerTest {
 
-    @Test
-    void setTask() {
-        final StateLogger stateLogger = new StateLogger();
-        stateLogger.setTask("1");
-    }
+    // @Test
+    // void setTask() {
+    //     final StateLogger stateLogger = new StateLogger();
+    //     stateLogger.setTask("1");
+    // }
 }
